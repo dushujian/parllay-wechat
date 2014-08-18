d85681fb555f0807329c749da8394b630f73b3b2
dushujian <499620890@qq.com>
2014-08-18 19:28:25 +0800

    new project

---
+++ upload/src/main/java/com/parllay/weichat/Demo.java
@@ -0,0 +1,48 @@
+package com.parllay.weichat;
+
+import com.parllay.util.ServiceUtil;
+import org.apache.log4j.Logger;
+
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by dushujian on 14-8-13.
+ */
+public class Demo {
+    public static void main(String[] args) throws IOException {
+
+        String param = "access_token=ACCESS_TOKEN&type=type&media=1";
+//        String param = "{\"access_token\":\"ACCESS_TOKEN\",\"type\":\"type\",\"media\":1}";
+        URL url = new URL("http://localhost:8080/parllay-wechat-1.0-SNAPSHOT/cgi-bin/media/upload");
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+        connection.setRequestProperty("Content-Length", Integer.toString(param.getBytes().length));
+//        connection.setRequestProperty("Content-Language", "en-US");
+
+        connection.setUseCaches (false);
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+
+        //Send request
+        DataOutputStream wr = new DataOutputStream (connection.getOutputStream ());
+        wr.write(param.getBytes());
+        wr.flush ();
+        wr.close ();
+
+        //Get Response
+        BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line;
+        while((line = rd.readLine()) != null) {
+            System.out.println(line);
+        }
+        rd.close();
+    }
+}
---
+++ upload/src/main/java/com/parllay/weichat/ServiceServlet.java
@@ -0,0 +1,65 @@
+package com.parllay.weichat;
+
+import com.parllay.util.ServiceUtil;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.json.simple.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by dushujian on 14-8-13.
+ */
+public class ServiceServlet extends HttpServlet {
+    Logger logger = Logger.getLogger(ServiceServlet.class);
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        PropertyConfigurator.configure("/home/dushujian/weichat/config/log4j.properties");
+        logger.info(req.getRequestURL());
+        JSONObject inmsg = new JSONObject();
+        JSONObject outmsg = new JSONObject();
+        Set<Integer> mid = new HashSet<Integer>(); //存放不重复的媒体id
+
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(req.getInputStream()));
+//        String line = reader.readLine();
+
+        if(req.getParameter("access_token") != null &&
+                req.getParameter("type") != null &&
+                req.getParameter("media") != null)
+        {
+            String type = req.getParameter("type");
+            String media = req.getParameter("media");
+            String access_token = req.getParameter("access_token");
+            inmsg.put("type",type);
+            inmsg.put("media", media);
+            inmsg.put("access_token", access_token);
+            logger.info("The incoming parameters:" + inmsg);
+            outmsg.put("created_at", ServiceUtil.getStringDateShort());
+            outmsg.put("type",type);
+            outmsg.put("MEDIA_ID", ServiceUtil.RandomNum(mid));
+            logger.info("The output parameters:" + outmsg);
+            out.print(outmsg);
+        }
+
+        else
+        {
+            logger.info("The parameter passed is not complete！");
+            outmsg.put("errcode", 40004);
+            outmsg.put("errmsg","invalid media type");
+            logger.info("The output parameters:" + outmsg);
+            out.print(outmsg);
+        }
+
+        out.flush();
+
+    }
+}
