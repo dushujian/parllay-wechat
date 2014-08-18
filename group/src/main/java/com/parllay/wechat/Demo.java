2a04898fc7bf64cb7adc7a80cae34142630789b8
dushujian <499620890@qq.com>
2014-08-18 19:26:37 +0800

    new project

---
+++ group/src/main/java/com/parllay/wechat/Demo.java
@@ -0,0 +1,45 @@
+package com.parllay.wechat;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by dushujian on 14-8-13.
+ */
+public class Demo {
+    public static void main(String[] args) throws IOException {
+
+//        String param = "{\"filter\":{\"group_id\":\"2\"},\"text\":{\"content\":\"CONTENT\"},\"msgtype\":\"text\"}";
+//        String param = "{\"articles\":[{\"thumb_media_id\":\"1\",\"author\":\"xxx\",\"title\":\"happy\",\"content\":\"content\"},{\"thumb_media_id\":\"1\",\"author\":\"xxx\",\"title\":\"happy\",\"content\":\"content\"}]}";
+        String param = "{\"group\":{\"name\":\"test\"}}";
+        URL url = new URL("http://localhost:8080/parllay-wechat-1.0-SNAPSHOT/cgi-bin/groups/create?access_token=ACCESS_TOKEN");
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+        connection.setRequestProperty("Content-Length", Integer.toString(param.getBytes().length));
+//        connection.setRequestProperty("Content-Language", "en-US");
+
+        connection.setUseCaches (false);
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+
+        //Send request
+        DataOutputStream wr = new DataOutputStream (connection.getOutputStream ());
+        wr.write(param.getBytes());
+        wr.flush ();
+        wr.close ();
+
+        //Get Response
+        BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line;
+        while((line = rd.readLine()) != null) {
+            System.out.println(line);
+        }
+        rd.close();
+    }
+}
---
+++ group/src/main/java/com/parllay/wechat/ServiceServlet.java
@@ -0,0 +1,75 @@
+package com.parllay.wechat;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import java.io.BufferedReader;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+/**
+ * Created by dushujian on 14-8-18.
+ */
+public class ServiceServlet extends HttpServlet {
+    Logger logger = Logger.getLogger(ServiceServlet.class);
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        PropertyConfigurator.configure("/home/dushujian/Desktop/group/config/log4j.properties");
+        logger.info(req.getRequestURL());
+        BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
+        String data = br.readLine();
+        br.close();
+        int success = 1; //标志参数是否传递正确
+        logger.info("The input parameters:" + data);
+        JSONObject childoutmsg = new JSONObject();
+        JSONObject outmsg = new JSONObject();
+        JSONObject name = null;
+
+        if(StringUtils.isEmpty(req.getParameter("access_token")))
+        {
+            success = 0;
+        }
+        JSONObject group = (JSONObject)JSONValue.parse(data);
+        if(!group.containsKey("group"))
+        {
+            success = 0;
+        }
+        else
+        {
+            name = (JSONObject)group.get("group");
+            if(!name.containsKey("name"))
+            {
+                success = 0;
+            }
+        }
+
+        if (1 == success)
+        {
+            childoutmsg.put("id", new Random().nextInt(1000));
+            childoutmsg.put("name", name.get("name"));
+            outmsg.put("group", childoutmsg);
+            logger.info("The output parameters:" + outmsg);
+            out.print(outmsg);
+        }
+
+        else
+        {
+            outmsg.put("errcode", 40013);
+            outmsg.put("errmsg", "invalid appid");
+            logger.info("The output parameters:" + outmsg);
+            out.print(outmsg);
+        }
+    }
+}
