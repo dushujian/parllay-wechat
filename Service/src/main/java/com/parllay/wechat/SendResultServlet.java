cf8a19c8efdd92c74da90501b12b74489e80b17d
dushujian <499620890@qq.com>
2014-08-21 10:04:01 +0800

    new project

---
+++ Service/src/main/java/com/parllay/wechat/SendResultServlet.java
@@ -0,0 +1,26 @@
+package com.parllay.wechat;
+
+import com.parllay.util.ServiceUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by dushujian on 14-8-21.
+ */
+public class SendResultServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.print(ServiceUtil.sendResult());
+        out.flush();
+    }
+}
