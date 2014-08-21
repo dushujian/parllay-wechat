aa11b58ee2b330523891581eb7a3fb26e7e978bb
dushujian <499620890@qq.com>
2014-08-21 14:22:01 +0800

    new project

--- Client/src/main/java/com/parllay/client/TimingSend.java
+++ Client/src/main/java/com/parllay/client/TimingSend.java
@@ -3,7 +3,6 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Timer;
 /**
