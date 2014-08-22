a9273043562464e8b122f334f4ca44741e287a2d
dushujian <499620890@qq.com>
2014-08-22 12:01:46 +0800

    new project

---
+++ Client/src/main/java/com/parllay/client/Message.java
@@ -0,0 +1,84 @@
+package com.parllay.client;
+
+import com.parllay.util.ClientUtil;
+
+/**
+ * Created by dushujian on 14-8-22.
+ */
+public class Message {
+    public static String textMsg(int msgId){
+        return  " <xml>\n" +
+                " <ToUserName><![CDATA[toUser]]></ToUserName>\n" +
+                " <FromUserName><![CDATA[fromUser]]></FromUserName> \n" +
+                " <CreateTime>" + ClientUtil.getStringDateShort() + "</CreateTime>\n" +
+                " <MsgType><![CDATA[text]]></MsgType>\n" +
+                " <Content><![CDATA[this is a test]]></Content>\n" +
+                " <MsgId>"+msgId+"</MsgId>\n" +
+                " </xml>";
+    }
+    public  static  String imgMsg(int msgId)
+    {
+        return "<xml>\n" +
+                " <ToUserName><![CDATA[toUser]]></ToUserName>\n" +
+                " <FromUserName><![CDATA[fromUser]]></FromUserName>\n" +
+                " <CreateTime>" + ClientUtil.getStringDateShort() + "</CreateTime>\n" +
+                " <MsgType><![CDATA[image]]></MsgType>\n" +
+                " <PicUrl><![CDATA[this is a url]]></PicUrl>\n" +
+                " <MediaId><![CDATA[media_id]]></MediaId>\n" +
+                " <MsgId>"+msgId+"</MsgId>\n" +
+                " </xml>";
+    }
+    public static String voiceMsg(int msgId)
+    {
+        return "<xml>\n" +
+                "<ToUserName><![CDATA[toUser]]></ToUserName>\n" +
+                "<FromUserName><![CDATA[fromUser]]></FromUserName>\n" +
+                " <CreateTime>" + ClientUtil.getStringDateShort() + "</CreateTime>\n" +
+                "<MsgType><![CDATA[voice]]></MsgType>\n" +
+                "<MediaId><![CDATA[media_id]]></MediaId>\n" +
+                "<Format><![CDATA[Format]]></Format>\n" +
+                " <MsgId>"+msgId+"</MsgId>\n" +
+                "</xml>";
+    }
+    public static String videoMsg(int msgId)
+    {
+        return "<xml>\n" +
+                "<ToUserName><![CDATA[toUser]]></ToUserName>\n" +
+                "<FromUserName><![CDATA[fromUser]]></FromUserName>\n" +
+                " <CreateTime>" + ClientUtil.getStringDateShort() + "</CreateTime>\n" +
+                "<MsgType><![CDATA[video]]></MsgType>\n" +
+                "<MediaId><![CDATA[media_id]]></MediaId>\n" +
+                "<ThumbMediaId><![CDATA[thumb_media_id]]></ThumbMediaId>\n" +
+                " <MsgId>"+msgId+"</MsgId>\n" +
+                "</xml>";
+    }
+
+    public  static String locationMsg(int msgId)
+    {
+        return "<xml>\n" +
+                "<ToUserName><![CDATA[toUser]]></ToUserName>\n" +
+                "<FromUserName><![CDATA[fromUser]]></FromUserName>\n" +
+                " <CreateTime>" + ClientUtil.getStringDateShort() + "</CreateTime>\n" +
+                "<MsgType><![CDATA[location]]></MsgType>\n" +
+                "<Location_X>23.134521</Location_X>\n" +
+                "<Location_Y>113.358803</Location_Y>\n" +
+                "<Scale>20</Scale>\n" +
+                "<Label><![CDATA[位置信息]]></Label>\n" +
+                " <MsgId>"+msgId+"</MsgId>\n" +
+                "</xml> ";
+    }
+
+    public static String linkMsg(int msgId)
+    {
+        return "<xml>\n" +
+                "<ToUserName><![CDATA[toUser]]></ToUserName>\n" +
+                "<FromUserName><![CDATA[fromUser]]></FromUserName>\n" +
+                " <CreateTime>" + ClientUtil.getStringDateShort() + "</CreateTime>\n" +
+                "<MsgType><![CDATA[link]]></MsgType>\n" +
+                "<Title><![CDATA[公众平台官网链接]]></Title>\n" +
+                "<Description><![CDATA[公众平台官网链接]]></Description>\n" +
+                "<Url><![CDATA[url]]></Url>\n" +
+                " <MsgId>"+msgId+"</MsgId>\n" +
+                "</xml> ";
+    }
+}
---
+++ Client/src/main/java/com/parllay/util/ClientUtil.java
@@ -0,0 +1,90 @@
+package com.parllay.util;
+
+
+import com.parllay.client.Message;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+import java.util.Set;
+
+/**
+ * Created by dushujian on 14-8-15.
+ */
+public class ClientUtil {
+    //产生随机数
+    public static Integer RandomNum(Set<Integer> mid)
+    {
+        int length = mid.size();
+        int i = 0;
+        Random random = new Random();
+        do {
+            i = random.nextInt();
+            mid.add(i);
+        }while(mid.size() == length);
+        return  i;
+    }
+
+    //产生指定数量的消息
+    public  static String msgs(int num, String type,Set mid)
+    {
+        StringBuilder mes = new StringBuilder();
+        if(type.equals("text"))
+        {
+            for(int i = 0; i < num; i++)
+            {
+                mes.append(Message.textMsg(RandomNum(mid)));
+            }
+        }
+
+        if(type.equals("iamge"))
+        {
+            for(int i = 0; i < num; i++)
+            {
+                mes.append(Message.imgMsg(RandomNum(mid)));
+            }
+        }
+
+        if(type.equals("vioce"))
+        {
+            for(int i = 0; i < num; i++)
+            {
+                mes.append(Message.voiceMsg(RandomNum(mid)));
+            }
+        }
+
+        if (type.equals("video"))
+        {
+            for(int i = 0; i < num; i++)
+            {
+                mes.append(Message.videoMsg(RandomNum(mid)));
+            }
+        }
+
+        if(type.equals("location"))
+        {
+            for(int i = 0; i < num; i++)
+            {
+                mes.append(Message.locationMsg(RandomNum(mid)));
+            }
+        }
+
+        if(type.equals("link"))
+        {
+            for(int i = 0; i < num; i++)
+            {
+                mes.append(Message.linkMsg(RandomNum(mid)));
+            }
+        }
+
+        return mes.toString();
+
+    }
+    //当前时间
+    public static String getStringDateShort() {
+        Date currentTime = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateString = formatter.format(currentTime);
+        return dateString;
+    }
+}
